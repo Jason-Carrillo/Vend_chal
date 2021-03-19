aff3f1f7c7f8f9768279a5067e5a0cec19e50d38
Jason Carrillo <jason.carrillo182@gmail.com>
2021-03-18 20:58:29 -0500

    added row to print out the letter that is presnted for its index instead of number

--- vendingChall.java
+++ vendingChall.java
@@ -1,10 +1,14 @@
+import java.util.ArrayList;
+
 import jdk.internal.joptsimple.internal.Rows;
 
 public class vendingChall {
 
+    static char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+    
     public static void rowSetter(int x){
-        for(int i = 1; i <= x; i++){
-            System.out.print(i);
+        for(int i = 0; i <= x; i++){
+            System.out.print(alphabet[i]);
         }
         System.out.println();
     }
@@ -15,10 +19,14 @@ public class vendingChall {
         }
         System.out.println();
     }
+
+    public static void rowColumnPrinter(int x, int y){
+        rowSetter(x);
+        columnSetter(y);
+    }
     public static void main(String[] args) {
 
-        rowSetter(5);
-        ColumnSetter(4);
+        rowColumnPrinter(8, 9);
 
         System.out.println("Please enter Row (Letter) for desired candy.");
         System.out.println("Please enter Column (Number) for desired candy.");
