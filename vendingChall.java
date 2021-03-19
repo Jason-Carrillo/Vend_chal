8b295abc3f1bb0824fca76e6d805a74fb9f6e28e
Jason Carrillo <jason.carrillo182@gmail.com>
2021-03-18 20:49:54 -0500

    added spacing in row setter

--- vendingChall.java
+++ vendingChall.java
@@ -1,16 +1,16 @@
+import jdk.internal.joptsimple.internal.Rows;
+
 public class vendingChall {
 
-    public void rowSetter(int x){
+    public static void rowSetter(int x){
         for(int i = 0; i <= x; i++){
-
+            System.out.print(i);
         }
+        System.out.println();
     }
     public static void main(String[] args) {
 
-        
-
-
-        System.out.print("x");
+        rowSetter(5);
 
         System.out.println("Please enter Row (Letter) for desired candy.");
         System.out.println("Please enter Column (Number) for desired candy.");
