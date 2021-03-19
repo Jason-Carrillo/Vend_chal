e752c37b498a818b86d3b89923e5b1a93e615ae8
Jason Carrillo <jason.carrillo182@gmail.com>
2021-03-18 20:51:55 -0500

    fixed typo in column setter

--- vendingChall.java
+++ vendingChall.java
@@ -9,7 +9,7 @@ public class vendingChall {
         System.out.println();
     }
 
-    public static void ColumnSetter(int x){
+    public static void columnSetter(int x){
         for(int i = 0; i <= x; i++){
             System.out.println(i);
         }
