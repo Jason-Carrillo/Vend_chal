ea90b930c9b92ffcc01c7c055d7059e25f388f88
Jason Carrillo <jason.carrillo182@gmail.com>
2021-03-18 20:47:07 -0500

    added row setter

--- vendingChall.java
+++ vendingChall.java
@@ -1,5 +1,17 @@
 public class vendingChall {
+
+    public void roewSetter(int x){
+        for(int i = 0; i <= x; i++){
+
+        }
+    }
     public static void main(String[] args) {
+
+        
+
+
+        System.out.print("x");
+
         System.out.println("Please enter Row (Letter) for desired candy.");
         System.out.println("Please enter Column (Number) for desired candy.");
     }
