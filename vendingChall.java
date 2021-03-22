31c99ed52a3eef94ba2ce53f3afff4f06322e25f
Jason Carrillo <jason.carrillo182@gmail.com>
2021-03-22 12:27:33 -0600

    fixed typo in item.size changed to ITEMS.size

--- vendingChall.java
+++ vendingChall.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
@@ -56,6 +58,75 @@ public class vendingChall extends JFrame{
                 return "JSON file (*.json)";
             }
         });
+
+        String filename;
+
+        int ret  = fileChooser.showOpenDialog(null);
+        if(ret == JFileChooser.APPROVE_OPTION){
+            filename = fileChooser.getSelectedFile().getName();
+        } else {
+            return;
+        }
+    
+
+        // Create this method 
+        loadJSONFile(filename);
     }
+
+    private void loadJSONFile(String filename){
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filename));
+
+            StringBuilder json = new StringBuilder();
+            String line;
+
+            while((line = br.readLine()) != null){
+                json.append(line);
+            }
+
+            JSONObject jsonObject = new JSONObject(json.toString());
+
+            int rows = jsonObject.getJSONObject("config").getInt("rows");
+            int columns = jsonObject.getJSONObject("config").getInt("columns");
+
+            ArrayList<Item> items = new ArrayList<>();
+
+            JSONArray itemsArray = jsonObject.getJSONArray("items");
+
+            for(int i = 0; i < itemsArray.length(); i++){
+                JSONObject itemObject = itemsArray.getJSONObject(i);
+
+                Item item = new Item(
+                    itemObject.getString("name"),
+                    itemObject.getInt("quantity"),
+                    itemObject.getString("price")
+                );
+
+                items.add(item);
+            }
+
+            if(rows * columns < items.size()){
+                System.err.println( items.size() + " items do not fit inside " + rows + " rows, and " + columns + " columns ");
+                return;
+            }
+
+            configRow = rows;
+            configColumn = columns;
+            this.items.clear();
+            this.items.addAll(items);
+
+            //TODO- create this method tha twill load items to the ui.
+            loadToUI()
+
+            System.out.println("Items: " + items.size() + " Row x Column: " + configRow + " x " + configColumn);
+        
+        }
+
+
+    }
+
+
+
+
 
 }
