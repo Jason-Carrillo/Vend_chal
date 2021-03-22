f972076919b50603abe761ba690f415742dcaa31
Jason Carrillo <jason.carrillo182@gmail.com>
2021-03-22 11:40:45 -0600

    removed testing row, columns

--- vendingChall.java
+++ vendingChall.java
@@ -1,43 +1,26 @@
 import java.util.ArrayList;
 
-import jdk.internal.joptsimple.internal.Rows;
-
-public class vendingChall {
-
-    static char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
-
-    static int rows = 4;
-
-    static int columns = 8;
-
-    static ArrayList<Object> machineBoard;
+import javax.swing.JFormattedTextField;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 
+public class vendingChall extends JFrame{
+
+    int configRow;
+    int configColumn;
+
+    ArrayList<Item> items = new ArrayList<>();
+
+    ArrayList<Jlabel> itemsList = new ArrayList<>();
+
+    JPanel itemsPanel = new JPanel();
+
+    JFormattedTextField formatted;
+
+    JButton buttonCheckout;
     
-    public static void rowSetter(int x){
-
-        for(int i = -1; i <= x; i++){
-
-            if(i == -1) {
-            System.out.print("+");
-            } else {
-            System.out.print(alphabet[i]);
-        }
-        }
-        System.out.println();
-    }
-
-    public static void columnSetter(int x){
-        for(int i = 1; i <= x; i++){
-            System.out.println(i);
-        }
-        System.out.println();
-    }
-
-    public static void rowColumnPrinter(int x, int y){
-        rowSetter(x);
-        columnSetter(y);
-    }
+    
     public static void main(String[] args) {
 
         for(int i = 0; i <= rows; i++){
